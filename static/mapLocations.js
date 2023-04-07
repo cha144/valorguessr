6778286eb29ce4308bd7909f1e72ae67044d35a2
charles <spamchar1es0@gmail.com>
2023-04-06 19:36:55 -0700

    add x,y coordinates for each location of ascent screenshots

--- static/mapLocations.js
+++ static/mapLocations.js
@@ -1,4 +1,96 @@
 const locations = {
-Ascent1: {x: 10, y: 20},
+//Ascent
+Ascent1: {x: 328, y: 820},
+Ascent2: {x: 345, y: 765},
+Ascent3: {x: 88, y: 700},
+Ascent4: {x: 237, y: 772},
+Ascent5: {x: 298, y: 695},
+Ascent6: {x: 108, y: 797},
+Ascent7: {x: 96, y: 746},
+
+//Bind
+Bind1: {x: 328, y: 820},
+Bind2: {x: 328, y: 820},
+Bind3: {x: 328, y: 820},
+Bind4: {x: 328, y: 820},
+Bind5: {x: 328, y: 820},
+Bind6: {x: 328, y: 820},
+Bind7: {x: 328, y: 820},
+Bind8: {x: 328, y: 820},
+Bind9: {x: 328, y: 820},
+Bind10: {x: 328, y: 820},
+Bind11: {x: 328, y: 820},
+
+//Breeze
+Breeze1: {x: 328, y: 820},
+Breeze2: {x: 328, y: 820},
+Breeze3: {x: 328, y: 820},
+Breeze4: {x: 328, y: 820},
+Breeze5: {x: 328, y: 820},
+Breeze6: {x: 328, y: 820},
+Breeze7: {x: 328, y: 820},
+Breeze8: {x: 328, y: 820},
+Breeze9: {x: 328, y: 820},
+Breeze10: {x: 328, y: 820},
+Breeze11: {x: 328, y: 820},
+
+//Haven
+Haven1: {x: 328, y: 820},
+Haven2: {x: 328, y: 820},
+Haven3: {x: 328, y: 820},
+Haven4: {x: 328, y: 820},
+Haven5: {x: 328, y: 820},
+Haven6: {x: 328, y: 820},
+Haven7: {x: 328, y: 820},
+Haven8: {x: 328, y: 820},
+
+//Icebox
+Icebox1: {x: 328, y: 820},
+Icebox2: {x: 328, y: 820},
+Icebox3: {x: 328, y: 820},
+Icebox4: {x: 328, y: 820},
+Icebox5: {x: 328, y: 820},
+Icebox6: {x: 328, y: 820},
+Icebox7: {x: 328, y: 820},
+Icebox8: {x: 328, y: 820},
+Icebox9: {x: 328, y: 820},
+Icebox10: {x: 328, y: 820},
+
+//Lotus
+Lotus1: {x: 328, y: 820},
+Lotus2: {x: 328, y: 820},
+Lotus3: {x: 328, y: 820},
+Lotus4: {x: 328, y: 820},
+Lotus5: {x: 328, y: 820},
+Lotus6: {x: 328, y: 820},
+Lotus7: {x: 328, y: 820},
+Lotus8: {x: 328, y: 820},
+Lotus9: {x: 328, y: 820},
+
+//Pearl
+Pearl1: {x: 328, y: 820},
+Pearl2: {x: 328, y: 820},
+Pearl3: {x: 328, y: 820},
+Pearl4: {x: 328, y: 820},
+Pearl5: {x: 328, y: 820},
+Pearl6: {x: 328, y: 820},
+Pearl7: {x: 328, y: 820},
+Pearl8: {x: 328, y: 820},
+Pearl9: {x: 328, y: 820},
+Pearl10: {x: 328, y: 820},
+
+//Split
+Split1: {x: 328, y: 820},
+Split2: {x: 328, y: 820},
+Split3: {x: 328, y: 820},
+Split4: {x: 328, y: 820},
+Split5: {x: 328, y: 820},
+Split6: {x: 328, y: 820},
+Split7: {x: 328, y: 820},
+Split8: {x: 328, y: 820},
+Split9: {x: 328, y: 820},
+Split10: {x: 328, y: 820},
+Split11: {x: 328, y: 820};
+
 
 };
