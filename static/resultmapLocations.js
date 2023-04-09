d7a9cf7084560a185860643af919202c379ba0e2
charles <spamchar1es0@gmail.com>
2023-04-08 22:33:36 -0700

    added score result popup

---
+++ static/resultmapLocations.js
@@ -0,0 +1,82 @@
+const resultLocations = {
+//Ascent
+'Ascent1.png': {x: 328, y: 820},
+'Ascent2.png': {x: 345, y: 765},
+'Ascent3.png': {x: 88, y: 700},
+'Ascent4.png': {x: 237, y: 772},
+'Ascent5.png': {x: 298, y: 695},
+'Ascent6.png': {x: 108, y: 797},
+'Ascent7.png': {x: 96, y: 746},
+
+//Breeze
+'Breeze1.png': {x: 316, y: 532},
+'Breeze2.png': {x: 390, y: 620},
+'Breeze3.png': {x: 328, y: 820},
+'Breeze4.png': {x: 290, y: 782},
+'Breeze5.png': {x: 166, y: 815},
+'Breeze6.png': {x: 102, y: 616},
+'Breeze7.png': {x: 149, y: 625},
+'Breeze8.png': {x: 178, y: 558},
+'Breeze9.png': {x: 201, y: 722},
+'Breeze10.png': {x: 233, y: 790},
+'Breeze11.png': {x: 329, y: 832},
+
+//Haven
+'Haven1.png': {x: 316, y: 703},
+'Haven2.png': {x: 411, y: 723},
+'Haven3.png': {x: 406, y: 626},
+'Haven4.png': {x: 134, y: 662},
+'Haven5.png': {x: 101, y: 795},
+'Haven6.png': {x: 277, y: 708},
+'Haven7.png': {x: 178, y: 853},
+'Haven8.png': {x: 90, y: 793},
+
+//Icebox
+'Icebox1.png': {x: 300, y: 809},
+'Icebox2.png': {x: 91, y: 807},
+'Icebox3.png': {x: 152, y: 687},
+'Icebox4.png': {x: 101, y: 628},
+'Icebox5.png': {x: 268, y: 572},
+'Icebox6.png': {x: 368, y: 657},
+'Icebox7.png': {x: 362, y: 768},
+'Icebox8.png': {x: 401, y: 816},
+'Icebox9.png': {x: 415, y: 792},
+'Icebox10.png': {x: 259, y: 788},
+
+//Lotus
+'Lotus1.png': {x: 328, y: 820},
+'Lotus2.png': {x: 328, y: 820},
+'Lotus3.png': {x: 328, y: 820},
+'Lotus4.png': {x: 328, y: 820},
+'Lotus5.png': {x: 328, y: 820},
+'Lotus6.png': {x: 328, y: 820},
+'Lotus7.png': {x: 328, y: 820},
+'Lotus8.png': {x: 328, y: 820},
+'Lotus9.png': {x: 328, y: 820},
+
+//Pearl
+'Pearl1.png': {x: 248, y: 576},
+'Pearl2.png': {x: 342, y: 561},
+'Pearl3.png': {x: 416, y: 689},
+'Pearl4.png': {x: 390, y: 639},
+'Pearl5.png': {x: 247, y: 681},
+'Pearl6.png': {x: 207, y: 633},
+'Pearl7.png': {x: 78, y: 675},
+'Pearl8.png': {x: 233, y: 841},
+'Pearl9.png': {x: 238, y: 832},
+'Pearl10.png': {x: 265, y: 768},
+
+//Split
+'Split1.png': {x: 129, y: 815},
+'Split2.png': {x: 86, y: 745},
+'Split3.png': {x: 95, y: 643},
+'Split4.png': {x: 164, y: 602},
+'Split5.png': {x: 330, y: 574},
+'Split6.png': {x: 352, y: 701},
+'Split7.png': {x: 339, y: 798},
+'Split8.png': {x: 252, y: 818},
+'Split9.png': {x: 299, y: 748},
+'Split10.png': {x: 223, y: 724},
+'Split11.png': {x: 406, y: 653}
+
+};
