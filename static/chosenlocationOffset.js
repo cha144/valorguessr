1a7eabdc2b35eef661c66dd1de0802b786ad688b
charles <spamchar1es0@gmail.com>
2023-04-11 00:23:35 -0700

    v1.0 finish

---
+++ static/chosenlocationOffset.js
@@ -0,0 +1,10 @@
+const offset = {
+'Ascent': {x: 500, y: 500}
+'Icebox': {x: 500, y: 500}
+'Pearl': {x: 500, y: 500}
+'Bind': {x: 500, y: 500}
+'Breeze': {x: 500, y: 500}
+'Pearl': {x: 500, y: 500}
+'Lotus': {x: 500, y: 500}
+'Haven': {x: 500, y: 500}
+}
