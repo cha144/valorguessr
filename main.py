d0d493b1f827f1581fedeb62591ca0ca11feb0c1
charles <spamchar1es0@gmail.com>
2023-04-12 23:29:20 -0700

    add mobile file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    user_agent = request.user_agent
+    if user_agent.platform == 'android' or user_agent.platform == 'iphone' or user_agent.platform == 'ipad':
+        return render_template('mobile.html')
+    else:
+        return render_template("home.html")
 
 @app.route("/about")
 def about():
