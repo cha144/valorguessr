af1fe1478f19409a8f544e88ae7424d0874dce39
charles <spamchar1es0@gmail.com>
2023-04-12 23:37:21 -0700

    redirects mobile users to the mobile message if attempting to access any webpage of the website

--- main.py
+++ main.py
@@ -18,10 +18,30 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    user_agent = request.headers.get('User-Agent')
+    user_agent = user_agent.lower()
+
+    if "iphone" in user_agent:
+        return render_template('mobile.html')
+    elif "android" in user_agent:
+        return render_template('mobile.html')
+    elif "ipad" in user_agent:
+        return render_template('mobile.html')
+    else:
+        return render_template("about.html")
 
 @app.route("/play")
 def play():
-    return render_template("play.html")
+    user_agent = request.headers.get('User-Agent')
+    user_agent = user_agent.lower()
+
+    if "iphone" in user_agent:
+        return render_template('mobile.html')
+    elif "android" in user_agent:
+        return render_template('mobile.html')
+    elif "ipad" in user_agent:
+        return render_template('mobile.html')
+    else:
+        return render_template("play.html")
 
 
